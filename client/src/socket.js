c35255d7d03beb0895c4e9f64a0b56ad45c02506
Jinit Mewada <jinitmewada41@gmail.com>
2026-04-16 20:40:22 +0530

    Socket URl Change

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://code-interview-plateform-backend.onrender.com", {
     transports: ["websocket"],
 });
 
