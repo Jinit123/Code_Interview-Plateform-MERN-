2a89ee75a954fd3aff8d0c6d97cdb35a0d92ccd7
Jinit Mewada <jinitmewada41@gmail.com>
2026-04-17 13:10:14 +0530

    Toast Added

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://code-interview-plateform-backend.onrender.com", {
+const socket = io(import.meta.env.VITE_API_URL, {
     transports: ["websocket"],
 });
 
